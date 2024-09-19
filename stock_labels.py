c6d6d99f5c51a445a0804be088262a91a796c8ff
weihan <30489517+weihanAu@users.noreply.github.com>
2024-09-19 14:15:01 +1000

    Update stock_labels.py

--- stock_labels.py
+++ stock_labels.py
@@ -66,11 +66,11 @@ def generate_labels():
 
 # Initialize the application
 app = tk.Tk()
-app.title("Label Generator")
+app.title("Label Generator - ARZ foodservice")
 
 file_path = ""
 
-app.geometry("400x300")
+app.geometry("400x400")
 
 # Create a frame for the UI with padding
 frame = tk.Frame(app, padx=20, pady=20)
@@ -87,5 +87,23 @@ label.pack(pady=10)
 # Generate Labels Button
 generate_button = tk.Button(app, text="Generate Labels", command=generate_labels)
 generate_button.pack(pady=10)
+
+# Text below the Generate Labels button
+info_label = tk.Label(app, text=
+    "Please don't modify the CSV file downloaded from SAGE. Coz it has the Default structure as below: \n"
+    "\n"
+    "row 1 is the CSV file title meta.\n"
+    "\n"
+    "row 2 is the column description.\n"
+    "\n"
+    "Column G is the quantity that the customer ordered.\n"
+    "\n"
+    "Column H is the number of items in one box.\n"
+    "\n"
+    "Column I has the value '00000000' is for weighted items.\n"
+    "\n",
+      wraplength=300)
+info_label.pack(pady=5)
+
 
 app.mainloop()
